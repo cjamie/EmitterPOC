e110bcca1a89cc6363ca9e69d8ad89fb49460749
Jamie Chu <jamiechu@Jamies-MacBook-Pro.local>
2019-03-23 15:03:06 -0700

    initial commit

---
+++ EmitterPOC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmitterPOC
+//
+//  Created by Jamie Chu on 3/23/19.
+//  Copyright © 2019 Jamie Chu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
