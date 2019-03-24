290857d28f00c4a4dec0abf6ff65a3c9bc41fb3a
Jamie Chu <jamiechu@Jamies-MacBook-Pro.local>
2019-03-23 17:00:16 -0700

    finished

--- EmitterPOC/ViewController.swift
+++ EmitterPOC/ViewController.swift
@@ -8,13 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet weak var helloLabel: UILabel!
+    @IBOutlet weak var appreciationLabel: UILabel!
+    @IBOutlet weak var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+//            self.winkHelloLabel()
+            self.emitBasketBalls()
+        }
     }
 
-
+    // MARK: - Helpers
+    
+    private func winkHelloLabel() {
+        UIView.animate(withDuration: 0.2, animations: {
+            self.helloLabel.transform = CGAffineTransform(scaleX: 1, y: 0.1)
+        }, completion: { _ in
+            self.resetWinkLabels()
+        })
+    }
+    
+    private func resetWinkLabels() {
+        UIView.animate(withDuration: 0.2, animations: {
+            self.helloLabel.transform = .identity
+        }) { _ in
+            print("-=- we will emit basketballs here")
+        }
+    }
+    
+    private func emitBasketBalls() {
+        //  this is a layer that will be added into the emitter layer
+//        let emitter = CAEmitterLayer()
+        let emitter = Emitter.get(with: #imageLiteral(resourceName: "basketball"))
+        // we want it at the center of the view
+        emitter.emitterPosition = CGPoint(x: view.frame.width/2, y: 0)
+        emitter.emitterSize = CGSize(width: view.frame.width, height: 0)
+        view.layer.addSublayer(emitter)
+        
+        
+    }
+    
+    @IBAction private func didPressToggleButton(_ sender: UIButton) {
+        winkHelloLabel()
+    }
 }
 
